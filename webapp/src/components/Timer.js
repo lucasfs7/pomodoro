bae470f38d5465319271495def80dd895ed2c515
lucasfs7 <lucasfs7@gmail.com>
2017-05-12 01:15:44 -0300

    use immutable Map to handle Timer component's state

--- webapp/src/components/Timer.js
+++ webapp/src/components/Timer.js
@@ -2,14 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { compose, withState, withHandlers, lifecycle } from 'recompose'
 import { time as T, fullTime } from 'utils/time'
-import { mergeState } from 'utils/state'
+import { Map } from 'immutable'
 
 const Timer = ({ timer, startTimer, pauseTimer, stopTimer }) => (
   <div className='Timer'>
-    <h1>{ fullTime(timer.remaining) }</h1>
+    <h1>{ fullTime(timer.get('remaining')) }</h1>
     <button
-      onClick={ startTimer(timer.remaining) }
-      disabled={ !!timer.timeoutPID }>
+      onClick={ startTimer(timer.get('remaining')) }
+      disabled={ !!timer.get('timeoutPID') }>
       Start
     </button>
     <button onClick={ pauseTimer }>
@@ -29,44 +29,44 @@ Timer.propTypes = {
 const initialState = [
   'timer',
   'setTimer',
-  ({ time }) => ({ remaining: time, timeoutPID: null }),
+  ({ time }) => Map({ remaining: time, timeoutPID: null }),
 ]
 
 const lifecycleMethods = {
   componentWillReceiveProps({ time, timer, setTimer }) {
     if (time !== this.props.time) {
-      setTimer(mergeState({ ...timer, remaining: time }))
+      setTimer(timer.merge({ remaining: time }))
     }
   },
 }
 
-const startTimer = ({ time, setTimer, onFinish }) => (t) => {
+const startTimer = ({ time, timer, setTimer, onFinish }) => (t) => {
   const start = (t) => {
     if (t < T(1).seconds) {
-      setTimer(mergeState({ remaining: time, timeoutPID: false }))
+      setTimer(timer.merge({ remaining: time, timeoutPID: false }))
       if (onFinish) onFinish()
       return false
     }
     const remaining = t - T(1).seconds
     const timeoutFn = () => {
-      setTimer(mergeState({ remaining }))
+      setTimer(timer.merge({ remaining }))
       start(remaining)
     }
     const timeoutPID = setTimeout(timeoutFn, T(1).seconds)
-    setTimer(mergeState({ timeoutPID }))
+    setTimer(timer.merge({ remaining, timeoutPID }))
     return timeoutPID
   }
   return start.bind(null, t)
 }
 
 const pauseTimer = ({ timer, setTimer }) => () => {
-  clearTimeout(timer.timeoutPID)
-  setTimer(mergeState({ timeoutPID: null }))
+  clearTimeout(timer.get('timeoutPID'))
+  setTimer(timer.merge({ timeoutPID: null }))
 }
 
 const stopTimer = ({ time, timer, setTimer }) => () => {
-  clearTimeout(timer.timeoutPID)
-  setTimer(mergeState({ remaining: time, timeoutPID: null }))
+  clearTimeout(timer.get('timeoutPID'))
+  setTimer(timer.merge({ remaining: time, timeoutPID: null }))
 }
 
 export default compose(
