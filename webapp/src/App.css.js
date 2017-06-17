17852626affdfceed795f78bfd666865a203a258
lucasfs7 <lucasfs7@gmail.com>
2017-06-17 15:54:04 -0300

    remove ;

--- webapp/src/App.css.js
+++ webapp/src/App.css.js
@@ -3,7 +3,7 @@ import { css } from 'glamor'
 css.global('html, body, #root', {
   margin: '0',
   padding: '0',
-  fontFamily: `'Lato', sans-serif;`,
+  fontFamily: `'Lato', sans-serif`,
 })
 
 css.global('*', {
